83a993e93094ec583928aa6e0890948a4f3535a2
kihak12 <jeremyperret43@gmail.com>
2025-01-31 13:16:56 +0100

    revert

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,6 @@ import sass from "sass";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'],
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        404: './public/404.html',
-      },
-      output: {
-        assetFileNames: 'assets/[name][extname]'
-      }
-    }
-  },
   plugins: [react()],
   css: {
     preprocessors: {
