04d2499f20e632f259a7d52aef76b9fac4a19fe7
Jeremy <60700265+kihak12@users.noreply.github.com>
2025-02-17 22:52:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import sass from "sass";
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Kasa-Openclassrooms-Projet-11/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './public/404.html'
+      }
+    }
+  },
   plugins: [react()],
   css: {
     preprocessors: {
