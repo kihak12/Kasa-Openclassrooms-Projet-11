dd9a24607da42094d8092e7f99df067addd3fae8
Jeremy <60700265+kihak12@users.noreply.github.com>
2025-02-17 23:32:14 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import sass from "sass";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Kasa-Openclassrooms-Projet-11/',
   build: {
     outDir: 'dist',
     assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'],
@@ -12,9 +11,10 @@ export default defineConfig({
       input: {
         main: './index.html',
         404: './public/404.html',
+        redirects: './public/_redirects'
       },
       output: {
-        assetFileNames: 'assets/[name][extname]' // Structure de sortie
+        assetFileNames: 'assets/[name][extname]'
       }
     }
   },
