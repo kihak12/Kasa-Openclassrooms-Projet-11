58dc57faedb140e897421bc14cd1d75f7ed668fc
kihak12 <jeremyperret43@gmail.com>
2025-02-18 00:14:39 +0100

    last try

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
+    <HashRouter basename="/Kasa-Openclassrooms-Projet-11">
     <App />
+    </HashRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
         404: './public/404.html',
       },
       output: {
-        publicPath: '/Kasa-Openclassrooms-Projet-11/',
         assetFileNames: 'assets/[name][extname]'
       }
     }
